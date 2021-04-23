47f3e33b4c78b69ca5d340a3990a5a0930e7f92d
Kirill Varlamov <kirill@ongrid.pro>
2021-04-23 13:25:44 +0300

    Add support for multinet build (main + test)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-echo "Running BSC Geth..."
-cp -r /initialized_node_bsc_testnet/* /node/
-/usr/local/bin/geth --config /bsc_testnet/config.toml --datadir /node --networkid 97 --rpccorsdomain=* --wsorigins=*
+
+if [ -z "$NET_ID" ]; then
+  echo "NET_ID env not set. Using default NET_ID=56 (BSC mainnet)"
+  NET_ID=56
+fi
+
+if [ $NET_ID = "56" ]; then
+  NET_NAME="mainnet"
+elif [ $NET_ID = "97" ]; then
+  NET_NAME="testnet"
+else
+  echo "Unsupported network $NET_ID. Use either 56 (mainnet) or 97 (testnet)"
+  exit 1
+fi
+
+echo "Running on BSC $NET_NAME #$NET_ID"
+cp -r /initialized_node_bsc_$NET_NAME/* /node/
+/usr/local/bin/geth --config /bsc_$NET_NAME/config.toml --datadir /node --networkid $NET_ID --rpccorsdomain=* --wsorigins=*
